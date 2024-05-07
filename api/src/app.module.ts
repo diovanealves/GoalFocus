92f2d9e695dec61fd8fbb3da7f87174c56222ffb
Diovane Alves <alvdiovane@gmail.com>
2024-05-07 19:44:38 -0300

    feat: Adds global ValidationPipe to application

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './app/user/user.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), UserModule],
   controllers: [],
   providers: [],
 })
