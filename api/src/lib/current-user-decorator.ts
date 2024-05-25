1d2f12490216d0e96f0b9190db464f69a6fb58ce
Diovane Alves <alvdiovane@gmail.com>
2024-05-25 16:14:15 -0300

    feat: Add current user decorator for accessing user payload

---
+++ api/src/lib/current-user-decorator.ts
@@ -0,0 +1,13 @@
+import { ExecutionContext, createParamDecorator } from "@nestjs/common";
+
+export interface UserPayload {
+  sub: string;
+  email: string;
+}
+
+export const CurrentUser = createParamDecorator(
+  (_: unknown, context: ExecutionContext) => {
+    const request = context.switchToHttp().getRequest();
+    return request.user as UserPayload;
+  },
+);
