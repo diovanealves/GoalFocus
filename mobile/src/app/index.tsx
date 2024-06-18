ae93bdb22c459b82c2dadde4fee9da650e7105ca
Diovane Alves <alvdiovane@gmail.com>
2024-06-18 20:25:05 -0300

    feat(mobile): start coding the mobile application

---
+++ mobile/src/app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View className="flex-1 bg-black h-screen">
+      <Text>Home</Text>
+    </View>
+  );
+}
