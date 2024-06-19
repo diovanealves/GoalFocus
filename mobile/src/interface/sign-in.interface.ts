e91a7e233a45f33a4eaa6cb64e0346b49ad1eaa7
Diovane Alves <alvdiovane@gmail.com>
2024-06-19 20:23:08 -0300

    refactor(mobile): update validation error messages

--- mobile/src/interface/sign-in.interface.ts
+++ mobile/src/interface/sign-in.interface.ts
@@ -1,6 +1,9 @@
 import * as yup from "yup";
 
 export const SignInSchema = yup.object().shape({
-  email: yup.string().email("Email invalido").required("Email é obrigatório"),
-  password: yup.string().required("Password é obrigatório"),
+  email: yup
+    .string()
+    .email("Digite um email válido")
+    .required("O campo de email é obrigatório"),
+  password: yup.string().required("O campo de senha é obrigatório"),
 });
