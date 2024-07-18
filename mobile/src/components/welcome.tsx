599b7ed39579e2fa59242d3fb94cb3c92bf08480
Diovane Alves <alvdiovane@gmail.com>
2024-07-17 22:50:49 -0300

    feat(mobile): create hook to clear user tokens on sign out

--- mobile/src/components/welcome.tsx
+++ mobile/src/components/welcome.tsx
@@ -1,5 +1,10 @@
+import { FontAwesome } from "@expo/vector-icons";
 import { Text, View } from "react-native";
+
 import { Avatar } from "./avatar";
+import { Button } from "./button";
+
+import { useSignOut } from "../hooks/useSignOut";
 
 type WelcomeProps = {
   name: string;
@@ -7,17 +12,29 @@ type WelcomeProps = {
 
 export function Welcome({ name }: WelcomeProps) {
   const firstName = name.split(" ")[0];
+
+  function useHandleSignOut() {
+    useSignOut();
+  }
   return (
-    <View className="flex flex-row items-center space-x-3 p-4">
-      <Avatar name={firstName} />
-      <View>
-        <Text className="text-white text-base font-title">
-          Bem-vindo de volta, {firstName}!
-        </Text>
-        <Text className="opacity-60 text-white text-sm">
-          Vamos começar com seus objetivos.
-        </Text>
+    <View className="flex-row items-center justify-between p-4">
+      <View className="flex-row space-x-3">
+        <Avatar name={firstName} />
+        <View>
+          <Text className="text-white text-base font-title">
+            Bem-vindo de volta, {firstName}!
+          </Text>
+          <Text className="opacity-60 text-white text-sm">
+            Vamos começar com seus objetivos.
+          </Text>
+        </View>
       </View>
+
+      <Button className="bg-transparent" onPress={useHandleSignOut}>
+        <Button.Icon>
+          <FontAwesome name="sign-out" color="#FFFFFF" size={23} />
+        </Button.Icon>
+      </Button>
     </View>
   );
 }
