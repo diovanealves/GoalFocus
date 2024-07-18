3ab48fa9526a58783f940b323cd5a39b4d72a349
Diovane Alves <alvdiovane@gmail.com>
2024-07-17 21:49:13 -0300

    feat(mobile): add jwt-decode and @tanstack/react-query dependencies

--- mobile/src/app/_layout.tsx
+++ mobile/src/app/_layout.tsx
@@ -4,6 +4,7 @@ import {
   Inter_700Bold,
   useFonts,
 } from "@expo-google-fonts/inter";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Slot } from "expo-router";
 import { SafeAreaView, StatusBar } from "react-native";
 import Toast from "react-native-toast-message";
@@ -21,12 +22,12 @@ export default function Root() {
   }
 
   return (
-    <>
+    <QueryClientProvider client={new QueryClient()}>
       <SafeAreaView className="flex-1 bg-backgroud">
         <StatusBar backgroundColor="#0E0F11" />
         <Slot />
       </SafeAreaView>
       <Toast />
-    </>
+    </QueryClientProvider>
   );
 }
